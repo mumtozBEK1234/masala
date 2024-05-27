5da85e47a46de0beb696a381e0f301a97fa5fc3b
mumtozBEK1234 <email manzilingiz>
2024-05-27 14:43:24 +0300

    masala

---
+++ index.js
@@ -0,0 +1,163 @@
+// 1 masala
+// let n = 5
+
+// for (let i = 1 ; i <= n; i++ ) {
+//     console.log(i);
+    
+// }
+
+// 2 masala 
+
+// let n = 10
+//  for (let i = n ; i >= 1; i-- ) {
+//     console.log(i);
+//  }
+
+// 3-masala
+
+// let n = 10 
+
+// let result = 0
+// for (let i = 1 ; i <= n ; i++) {
+//   result  += i 
+// }
+// console.log(result);
+
+
+// 4-MASALA
+// let n = 10 
+
+// for(let i)
+// for-1 masala 
+
+// let k = 20; 
+// let n = 10;
+
+// for (let i = 0; i <= n; i++ ) {
+//     console.log(k);
+    
+// }
+
+// 5-masalasi
+
+// let n = +prompt("son kiriting");
+
+// for (let i =1; 1<=n; i+=2 ) {
+// if ( (1+n)/2*n && i%2==1 ) {
+//     console.log(i);
+// }
+    
+// }
+
+// 6-masalasi
+
+// let son = +prompt("Istagan soningizni kiriting (min: 15)")
+
+// for (let i = 1; i <= son; i++) {
+//     let output = "";
+//     if (i % 3 === 0) output += "Fizz";
+//     if (i % 5 === 0) output += "Buzz";
+//     console.log(output || i);
+//   }
+
+// 7-masalasi
+
+// let number =+prompt("son kiriting");
+
+// let sum =0;
+// let num = 0;
+
+// for(i=1; i<=number; i++ ){
+// if (number % i=== 0) {
+//     sum++;
+//     num+=i;
+// }
+// }
+
+// console.log(sum);
+// console.log(num);
+
+
+
+
+// for-1 masalasi
+
+// let k = +prompt("k sonini kiriting");
+// let n = +prompt("n sonini kiriting");
+
+// if (n<=0) {
+//     console.log(0 n dan katta bo'lishi kerak);
+// } 
+
+// let result=0;
+
+// for( let i=0; i < n; i++ ) {
+// result +=k;
+// }
+
+// console.log(${k } ning ko'paytiring ${n} ga ${result});
+
+
+// for-2 masala
+
+// let a = 2
+// let b = 4
+// for(a; a <= b; a++){
+//     console.log(a);
+// }
+
+
+
+// for-3 masala
+
+// let a = 2
+// let b = 5
+
+// for (let i = b - 1; i > a; i-- ) {
+//     console.log(i);
+    
+// } 
+
+
+
+// for-4masala
+
+// let kg = 10
+
+// for (let i = 1 ; i <= kg; i++ ) {
+//     console.log(i * kg);
+    
+// }
+
+// for-5 MASALA
+
+// const Kg = 1000; 
+
+// for (let i = 1; i <= 10; i++) {
+//     const son = i / 10;
+//     const result = son * Kg;
+//     console.log(`${son} kg konfetning narxi: ${result} som`);
+// }
+
+// for-6 masala
+
+// const kg = 1000;
+
+
+// for(let i = 10 ; i<= 20 ; i++) {
+//     const son = i / 10;
+//     const result =  son * kg 
+//     console.log(` ${son} kg konfetning narxi: ${result} so'm `);
+// }
+
+// for-7 masala
+// let a = 1
+// let b = 10
+// let result = 0 
+
+
+// for-8 masala (let i = a ; i <= b; i++ ) {
+//    result = result + i;
+    
+// }
+// console.log(result);
